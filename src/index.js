828038448eb697a9c77f662dd60353a4034c08e7
Oseyijotham <jothamoboh16@gmail.com>
2025-02-09 20:55:26 +0100

    Update App

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="oasis-tasks">
+      <BrowserRouter basename="Airboxify">
         <App />
       </BrowserRouter>
     </Provider>
